f605437fc6b9ba5138717df7593fdf08963038a4
Alejandro Adrian Macias Pantoja <alejandro.macias@encora.com>
2022-02-09 12:25:15 -0600

    Base project

---
+++ src/main/java/com/patitosoft/encora/patitosoftapp/PatitosoftAppApplication.java
@@ -0,0 +1,13 @@
+package com.patitosoft.encora.patitosoftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitosoftAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitosoftAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patitosoft/encora/patitosoftapp/PatitosoftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patitosoft.encora.patitosoftapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitosoftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
